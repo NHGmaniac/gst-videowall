7961af64d80886b84a876b38eb0e1f1ba7449ea4
Niels Bernlöhr <niels.bernloehr@web.de>
2017-08-30 21:46:56 +0200

    Added auto configuration endpoint

--- syncstream.py
+++ syncstream.py
@@ -100,7 +100,7 @@ def run_server(syncstream):
 
     # Server settings
     # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('127.0.0.1', 8081)
+    server_address = (syncstream.hostAddress, 8081)
     httpd = HTTPServer(server_address, createHandler(syncstream))
     print('running server...')
     httpd.serve_forever()
