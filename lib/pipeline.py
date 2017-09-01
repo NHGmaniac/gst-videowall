d5d540c33e51ac390341c3f2eb04eaf888611534
bene <nhg@posteo.de>
2017-09-01 22:51:11 +0200

    changed pipeline

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -12,7 +12,7 @@ class Pipeline(object):
         self.log = logging.getLogger('Pipeline')
         self.mm = MonitorManager()
         self.mm.load()
-        self.speed = "fast"
+        self.speed = "ultrafast"
 
     def configure(self):
         self.pipeline = None
@@ -28,7 +28,7 @@ class Pipeline(object):
         ! capsfilter caps="video/x-raw, width={width}, height={height}"
         ! tee name=t 
         ! queue 
-        ! x264enc tune=fastdecode speed-preset={speed} 
+        ! x264enc speed-preset={speed} 
         ! rtph264pay 
         ! rtpbin.send_rtp_sink_0
         
@@ -46,7 +46,7 @@ class Pipeline(object):
         t. 
         ! queue
         ! videocrop left={left} top={top} right={right} bottom={bottom}
-        ! x264enc tune=fastdecode speed-preset={speed} 
+        ! x264enc speed-preset={speed} 
         ! rtph264pay 
         ! rtpbin.send_rtp_sink_{id}
         
