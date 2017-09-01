9ecff33292e3e7ac041c3a25408459070cf4ba41
NHG <nhg@posteo.de>
2017-09-01 05:28:11 +0200

    random file streamer input via stream

--- randomfile.sh
+++ randomfile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd ../videofiles
+cd ~/videowall-videos/
 while true; do
     ls |sort -R |tail -1 |while read file; do
         gst-launch-1.0 filesrc location=$file ! decodebin ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=9999
