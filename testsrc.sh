af32e4a411658718853f92d2571c0380783fbf0d
bene <nhg@posteo.de>
2017-09-02 14:34:31 +0200

    test multiqueue

--- testsrc.sh
+++ testsrc.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gst-launch-1.0 videotestsrc ! videoscale ! video/x-raw ! rtprawpay ! udpsink host=127.0.0.1 sync=true port=9999
+gst-launch-1.0 -v videotestsrc ! videoscale ! video/x-raw ! rtpvrawpay ! udpsink host=127.0.0.1 sync=true port=9999
