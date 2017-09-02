42c70ed1d5cbb028c6d18858c180a1fe2ec9a29d
bene <nhg@posteo.de>
2017-09-02 14:55:57 +0200

    test multiqueue

--- randomfile.sh
+++ randomfile.sh
@@ -2,7 +2,7 @@
 cd ~/videowall-videos/
 while true; do
     ls |sort -R |tail -1 |while read file; do
-        gst-launch-1.0 -v filesrc location=$file ! decodebin ! videocsale ! video/x-raw, width=1920, height=1080 ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=9999
+        gst-launch-1.0 -v filesrc location=$file ! decodebin ! videoscale ! video/x-raw, width=1920, height=1080 ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 port=9999
     done
     sleep 5
 done
