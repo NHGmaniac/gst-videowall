7cca5cd5699989b865ceb5f6fb02239458c4d5e5
bene <nhg@posteo.de>
2017-09-02 13:37:14 +0200

    x264tuning

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -46,7 +46,7 @@ class Pipeline(object):
         t. 
         ! queue
         ! videocrop left={left} top={top} right={right} bottom={bottom}
-        ! x264enc speed-preset={speed} tune=zerolatency 
+        ! x264enc speed-preset={speed} tune=zerolatency intra-refresh=true
         ! rtph264pay 
         ! rtpbin.send_rtp_sink_{id}
         
