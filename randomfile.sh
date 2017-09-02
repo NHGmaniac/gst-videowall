f75a52f71eddb96df1fe9f14878bd1653cfe0215
bene <nhg@posteo.de>
2017-09-02 13:29:04 +0200

    x264tuning

--- randomfile.sh
+++ randomfile.sh
@@ -4,4 +4,5 @@ while true; do
     ls |sort -R |tail -1 |while read file; do
         gst-launch-1.0 filesrc location=$file ! decodebin ! x264enc ! rtph264pay ! udpsink host=10.128.0.1 sync=true port=9999
     done
+    sleep 5
 done
