63997e5de2beab4165271bcfee89cf405c402ade
bene <nhg@posteo.de>
2017-09-02 16:57:22 +0200

    reverting to h264

--- raspi-setup/setup-cont.sh
+++ raspi-setup/setup-cont.sh
@@ -19,7 +19,7 @@ else
 fi
 
 sh -c "TERM=linux setterm -clear all >/dev/tty0"
-sh -c "TERM=linux echo Hello VideoWall4! | figlet -c -w 150 >/dev/tty0"
+sh -c "TERM=linux echo Hello VideoWall5! | figlet -c -w 150 >/dev/tty0"
 sh -c "TERM=linux hostname -I | figlet -c -w 150 -W >/dev/tty0"
 mac=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address)
 sh -c "TERM=linux echo $mac | figlet -c -w 150 -W >/dev/tty0"
@@ -36,6 +36,7 @@ else
         git clone https://github.com/NHGmaniac/gst-videowall
         cd gst-videowall
     fi
+    git checkout master
 fi
 
 sh -c "TERM=linux echo Setup Completed | figlet -c -w 150 >/dev/tty0"
