7e68b34c63112e888c0d178113002e51c0796587
Tobias Wackenhut <tynsh@tynsh.moe>
2017-09-02 23:11:24 +0200

    fix

--- testsrc.sh
+++ testsrc.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gst-launch-1.0 -v videotestsrc ! videoscale ! videorate ! video/x-raw, width=1920, height=1080, framerate=25/1 ! x264enc ! rtph264pay ! udpsink host=127.0.0.1 sync=true port=9999
+gst-launch-1.0 -v videotestsrc ! videoscale ! videorate ! video/x-raw, width=1920, height=1080, framerate=25/1 ! udpsink host=127.0.0.1 sync=true port=9999
