982074058c4055f06f5ce7243cb56512ed0ddebb
NHG <nhg@posteo.de>
2017-11-15 20:14:57 +0100

    stuff

--- testsrc.sh
+++ testsrc.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gst-launch-1.0 -v videotestsrc ! videoscale ! video/x-raw, width=1920, height=1080, framerate=25/1 ! rtpvrawpay ! udpsink host=127.0.0.1 sync=true port=9999
+gst-launch-1.0 -v videotestsrc ! video/x-raw, format=I420, width=1920, height=1080, framerate=25/1 ! rtpvrawpay ! udpsink port=9999 host=127.0.0.1
