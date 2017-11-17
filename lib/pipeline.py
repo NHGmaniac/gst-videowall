575dbda5d7f5d1ff3e7dd4b612a6dfc4fe2f0f24
NHG <nhg@posteo.de>
2017-11-17 23:20:21 +0100

    static file for testing

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -52,7 +52,7 @@ class Pipeline(object):
         ! mq.
         mq.
         ! videocrop left={left} top={top} right={right} bottom={bottom}
-        ! x264enc speed-preset={speed} option-string="{option_string}" tune=zerolatency intra-refresh=true quantizer=20 pass=5
+        ! x264enc speed-preset={speed} option-string="{option_string}" tune=zerolatency intra-refresh=true quantizer=30 pass=5
         ! rtph264pay 
         ! rtpbin.send_rtp_sink_{id}
         
