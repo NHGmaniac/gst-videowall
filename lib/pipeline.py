6079cedd500994350de4947488e011882af90e8e
NHG <nhg@posteo.de>
2017-11-18 16:19:42 +0100

    changed rtcp time sync

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -55,7 +55,7 @@ class Pipeline(object):
         self.log = logging.getLogger('Pipeline')
         self.mm = MonitorManager()
         self.mm.load()
-        self.speed = "fast"
+        self.speed = "medium"
         self.option_string = "keyint=1"
 
     def configure(self):
