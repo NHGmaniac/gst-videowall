90ea98d68fc3875f7db0335f1ef156980a7c4e3a
NHG <nhg@posteo.de>
2017-12-02 15:16:15 +0100

    changed latency

--- lib/pipeline.py
+++ lib/pipeline.py
@@ -61,7 +61,7 @@ class Pipeline(object):
     def configure(self):
         self.pipeline = None
         pipelineTemplate = """
-        rtpbin name=rtpbin max-rtcp-rtp-time-diff=250
+        rtpbin name=rtpbin max-rtcp-rtp-time-diff=250 latency=2000
         
         intervideosrc channel=video
         ! decodebin
