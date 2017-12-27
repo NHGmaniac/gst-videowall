22d13b8c2f52a8ae6e816fac72faee2f6b32a587
kormarun <niels@derguhl.de>
2017-12-27 17:30:10 +0100

    Live config

--- testsrc.sh
+++ testsrc.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gst-launch-1.0 -v videotestsrc ! video/x-raw, format=I420, width=1920, height=1080, framerate=25/1 ! matroskamux ! tcpclientsink port=9999 host=127.0.0.1
+gst-launch-1.0 -v videotestsrc pattern=18 ! video/x-raw, format=I420, width=1920, height=1080, framerate=25/1 ! matroskamux ! tcpclientsink port=9999 host=127.0.0.1
